dced094625e15c1c19742a5c2e50369c10fb6f43
Etw <etw@users.noreply.github.com>
2015-05-31 21:37:58 +0300

    Add link to README and do gofmt

--- handlers.go
+++ handlers.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"errors"
 	"strings"
 
 	"crypto/rand"
 	"encoding/base64"
-	
+
 	"github.com/etw/pointapi"
 )
 
 type FeedMeta struct {
-	Title	string
-	ID		string
-	Href	string
+	Title string
+	ID    string
+	Href  string
 }
 
 func getRid() (string, error) {
@@ -30,7 +30,7 @@ func getRid() (string, error) {
 }
 
 func resRender(res http.ResponseWriter, rid *string, feed FeedMeta, body *map[string]interface{}) {
-	posts := (*body)["posts"].([]interface {})
+	posts := (*body)["posts"].([]interface{})
 	result, err := renderFeed(feed, posts)
 	if err != nil {
 		log.Printf("[ERROR] Failed to parse point response: %s\n", err)
@@ -42,7 +42,7 @@ func resRender(res http.ResponseWriter, rid *string, feed FeedMeta, body *map[st
 }
 
 func rootHandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, "")
+	fmt.Fprint(res, "https://github.com/etw/pointfeed/blob/develop/README.md")
 }
 
 func allHandler(api *pointapi.PointAPI) func(http.ResponseWriter, *http.Request) {
@@ -53,20 +53,20 @@ func allHandler(api *pointapi.PointAPI) func(http.ResponseWriter, *http.Request)
 			return
 		}
 		log.Printf("[INFO] {%s} %s %s\n", rid, req.Method, req.URL.Path)
-		
+
 		body, err := api.GetAll(0)
 		if err != nil {
 			log.Printf("[ERROR] {%s} Failed to get all posts: %s\n", rid, err)
 			res.WriteHeader(500)
 			return
 		}
-		
+
 		feed := FeedMeta{
 			Title: "All posts",
-			ID: "all",
-			Href: "https://point.im/all",
+			ID:    "all",
+			Href:  "https://point.im/all",
 		}
-		
+
 		resRender(res, &rid, feed, &body)
 	}
 }
@@ -80,18 +80,18 @@ func tagsHandler(api *pointapi.PointAPI) func(http.ResponseWriter, *http.Request
 			return
 		}
 		log.Printf("[INFO] {%s} %s %s\n", rid, req.Method, req.URL.Path)
-		
-		body, err := api.GetTags(0,tags)
+
+		body, err := api.GetTags(0, tags)
 		if err != nil {
 			log.Printf("[ERROR] {%s} Failed to get tagged posts: %s\n", rid, err)
 			res.WriteHeader(500)
 			return
 		}
-		
+
 		feed := FeedMeta{
 			Title: fmt.Sprintf("Tagged posts (%s)", strings.Join(tags, ", ")),
-			ID: fmt.Sprintf("tags:%s", strings.Join(tags, ",")),
-			Href: fmt.Sprintf("https://point.im/?tag=%s", strings.Join(tags, "&tag=")),
+			ID:    fmt.Sprintf("tags:%s", strings.Join(tags, ",")),
+			Href:  fmt.Sprintf("https://point.im/?tag=%s", strings.Join(tags, "&tag=")),
 		}
 
 		resRender(res, &rid, feed, &body)
